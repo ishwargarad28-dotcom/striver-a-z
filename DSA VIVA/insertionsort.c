8e2e21d239660a13d6d8577f2dd8279ef91cb458
Ishwar Garad <ishwargarad28@gmail.com>
2025-10-12 09:42:35 +0530

    insertio

---
+++ DSA VIVA/insertionsort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int arr[20], size, temp, j;
+    printf("enter the array size :");
+    scanf("%d", &size);
+    printf("enter the array element ;");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 1; i < size; i++)
+    {
+        temp = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d", arr[i]);
+    }
+    return 0;
+}
