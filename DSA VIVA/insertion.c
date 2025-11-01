4d006f889dc71b4ad2a14aebf7690b8d639a5d21
Ishwar Garad <ishwargarad28@gmail.com>
2025-11-02 00:02:35 +0530

    traversing

--- DSA VIVA/insertion.c
+++ DSA VIVA/insertion.c
@@ -1,26 +1,19 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    int array[20], size, num, position;
-    printf("enter the size of an array :");
-    scanf("%d", &size);
-    printf("enter the elements in an arrray :");
-    for (int i = 0; i < size; i++)
-    {
-        scanf("%d",&array[i]);
-    }
-    printf("enter the insert num :");
-    scanf("%d", &num);
-    printf("enter the position :");
-    scanf("%d", &position);
-    for (int i = size - 1; i >= position - 1; i--)
-    {
-        array[i + 1] = array[i];
-    }
-    array[position-1]=num;
-    size++;
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d", array[i]);
-    }
+    //display array 
+int arr[20],n=0,choice;
+while(1){
+    printf("1.insertion");
+    printf("2.deletion");
+    printf("3.search ");
+    printf("4.update");
+    printf("5.display");
+    printf("6.exit");
+
+    printf("enter your choice :");
+    scanf("%d",&choice);
+}
+   switch(choice)
+
 }
