c307fb911151a0babd9964516d4b780d886f290d
Ishwar Garad <ishwargarad28@gmail.com>
2026-03-02 09:59:32 +0530

    recursio 1

---
+++ DSA VIVA/bubble.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main (){
+    int i,j,temp,size;
+    int arr[20];
+    printf("enter the array size :");
+    scanf("%d",&size);
+    printf("enter the array elements :");
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<size-1;i++){
+        for(int j=0;j<size-1;j++){
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    for(int i=0;i<size;i++){
+        printf(" %d ",arr[i]);
+    }
+}
---
+++ DSA VIVA/recursion/program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+void printnum(int n)
+{
+    if (n == 1)
+    {
+        cout << "1\n";
+        return;
+    }
+    cout << n << " ";
+    printnum(n - 1);
+}
+int main()
+{
+    printnum(4);
+}
