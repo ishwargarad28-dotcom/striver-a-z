962015c94af7abeda4f16a528a285f7ad522b717
Ishwar Garad <ishwargarad28@gmail.com>
2026-08-18 21:44:22 +0530

    reverse array

---
+++ DSA/opertion on array/reverse_array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int reverseArray(int arr[],int size){
+  int start=0;
+  int end = size -1;
+  while(start<end){
+    swap(arr[start],arr[end]);
+    start++,end--;
+  }
+}
+int main(){
+  int arr[]={5,3,6,7,8,9};
+  int size=6;
+  reverseArray(arr,size);
+  for(int i=0;i<size;i++){
+    cout<<arr[i]<<" ";
+  }
+  cout<<endl;
+  return 0;
+}
